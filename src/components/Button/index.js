b8fa26cc633b63c01b718c156d43c6e9b42a2962
joaoPgp <joao.pgp26@gmail.com>
2021-12-06 14:50:15 -0300

    Create index.js

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Button({ type = 'primary', children, onClick }) {
+   const className = 'btn btn-' + type;
+
+   return (
+      <button className={className} onClick={onClick}>{children}</button>
+   );
+}
+
+export default Button;
