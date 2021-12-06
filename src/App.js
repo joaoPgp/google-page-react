3ef6adeb52e60c212566d70131d84de459be2e6a
joaoPgp <joao.pgp26@gmail.com>
2021-12-06 14:49:53 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+
+
+function App() {
+  const [isPortuguese, setIsPortuguese] = useState(false);
+
+  function translate() {
+    setIsPortuguese(!isPortuguese);
+
+  }
+
+  return (
+    <div className="container">
+      <Header isPortuguese={isPortuguese}></Header>
+      <Main isPortuguese={isPortuguese} translate={translate}></Main>
+      <Footer isPortuguese={isPortuguese}></Footer>
+    </div>
+  );
+}
+
+export default App;
