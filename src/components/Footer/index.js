9a2ffb6f84863856470bf04babcde46a2419c73d
joaoPgp <joao.pgp26@gmail.com>
2021-12-07 15:03:39 -0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,33 @@
 import './style.css'
 
-function Footer() {
+function Footer({ isPortuguese }) {
+
    return (
       <footer>
          <div id="pais" className="container-country">
-            Brazil
+            {isPortuguese ? 'Brazil' : 'Brasil'}
          </div>
          <nav class="container-about">
             <ul>
                <li>
-                  <a id="aviso" href="https://ads.google.com/intl/pt-BR_br/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2m--ahpm-0000000080-0000000001&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1">Advertising</a>
+                  <a id="aviso" href="https://ads.google.com/intl/pt-BR_br/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2m--ahpm-0000000080-0000000001&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1">
+                     {isPortuguese ? 'Advertising' : 'Publicidade'}
+                  </a>
                </li>
                <li>
-                  <a id="negocios" href="https://smallbusiness.withgoogle.com/intl/pt-BR_br/help/#!/">Business</a>
+                  <a id="negocios" href="https://smallbusiness.withgoogle.com/intl/pt-BR_br/help/#!/">
+                     {isPortuguese ? 'Business' : 'Negócios'}
+                  </a>
                </li>
                <li>
-                  <a id="sobre" href="https://about.google/?utm_source=google-BR&utm_medium=referral&utm_campaign=hp-footer&fg=1">About</a>
+                  <a id="sobre" href="https://about.google/?utm_source=google-BR&utm_medium=referral&utm_campaign=hp-footer&fg=1">
+                     {isPortuguese ? 'About' : 'Sobre'}
+                  </a>
                </li>
                <li>
-                  <a id="cpf" href="https://www.google.com/search/howsearchworks/?fg=1">How Search works</a>
+                  <a id="cpf" href="https://www.google.com/search/howsearchworks/?fg=1">
+                     {isPortuguese ? 'How Search works' : 'Como funciona a Pesquisa'}
+                  </a>
                </li>
             </ul>
          </nav>
@@ -26,13 +35,19 @@ function Footer() {
          <nav className="container-terms">
             <ul>
                <li>
-                  <a id="privacidade" href="https://policies.google.com/privacy?hl=pt-BR&fg=1">privicy</a>
+                  <a id="privacidade" href="https://policies.google.com/privacy?hl=pt-BR&fg=1">
+                     {isPortuguese ? 'privicy' : 'Privacidade'}
+                  </a>
                </li>
                <li>
-                  <a id="termos" href="https://policies.google.com/terms?hl=pt-BR&fg=1">terms</a>
+                  <a id="termos" href="https://policies.google.com/terms?hl=pt-BR&fg=1">
+                     {isPortuguese ? 'terms' : 'Termos'}
+                  </a>
                </li>
                <li>
-                  <a id="conf" href="https://www.google.com.br/preferences?hl=pt-BR&fg=1">settings</a>
+                  <a id="conf" href="https://www.google.com.br/preferences?hl=pt-BR&fg=1">
+                     {isPortuguese ? 'settings' : 'Configurações'}
+                  </a>
                </li>
             </ul>
          </nav>
